ef71cfb4ac2931da8718ec8b715aa0becb2d167e
its-ritu <paranjaperucha9@gmail.com>
2024-04-17 23:30:49 +0530

    added react components

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
 import "./App.css";
-// import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import Create from "./components/Create";
-// import Read from "./components/Read";
-// import Update from "./components/Update";
-// import Navbar from "./components/Navbar";
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <BrowserRouter>
-//         <Navbar />
-//         <Routes>
-//           <Route exact path="/" element={<Create />} />
-//           <Route path="/read" element={<Read />} />
-//           <Route path="/:id" element={<Update />} />
-//         </Routes>
-//       </BrowserRouter>
-//     </div>
-//   );
-// }
-
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Create from "./components/Create";
+import Read from "./components/Read";
+import Update from "./components/Update";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return(
+  return (
     <div className="App">
-      <h1>hello</h1>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Create />} />
+          <Route path="/read" element={<Read />} />
+          <Route path="/:id" element={<Update />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
+
+
+
+// function App() {
+//   return(
+//     <div className="App">
+//       <Navbar />
+//     </div>
+//   )
+// }
 export default App;
